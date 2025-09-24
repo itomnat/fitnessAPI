6e7b851ed882f4c7e9f38bbd3f1633e168314d52
itom.nat <itom.nat@gmail.com>
2025-09-25 06:34:03 +0800

    codes

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
     };
 
     useEffect(() => {
-        fetch(`${API_BASE_URL}/auth/verify`, {
+        fetch(`${API_BASE_URL}/auth?action=verify`, {
             headers: {
                 Authorization: `Bearer ${localStorage.getItem('token')}`
             }
